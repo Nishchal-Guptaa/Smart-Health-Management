c107193afa1fc12ae6e28423a9ccc2e255b23371
Aakash Raj Jindal <arjindal115@gmail.com>
2025-06-28 15:26:49 +0530

    routing dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,7 @@ const App = () => (
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="/forgot-password" element={<ForgotPassword />} />
+            
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
